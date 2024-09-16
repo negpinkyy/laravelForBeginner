6c4610fdcd30a3ff2ae96009853aa93f36a5c6b5
pinky winky <mac@MACs-MacBook-Pro.local>
2024-09-16 22:08:26 +0100

    Forms and CSRF

--- routes/web.php
+++ routes/web.php
@@ -3,28 +3,37 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-
 Route::get('/', function () {
-// $jobs=Job::all();
-// dd($jobs);
-
-   return view('home');
+    return view('home');
 });
-Route::get('/jobs', function ()  {
-$jobs=Job::with('employer')->cursorPaginate(3);
-//$jobs=Job::with('employer')->paginate(3);
-    return view(
-  'jobs',[
-        'jobs'=>$jobs,
-       ]);
-});
-Route::get('/jobs/{id}', function ($id)  {
 
-    $job =Job::find($id);
-    // dd($job);
-    return view('job', ['job' => $job]);
+Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+
+    return view('jobs.index', [
+        'jobs' => $jobs
+    ]);
+});
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+Route::Post('/jobs/', function () {
+   //  request()
+   Job::create([
+    'title'=>'Frontend',
+    'employer_id'=>1,
+    'salary'=>'$30,000'
+   ]);
+   return redirect('/jobs');
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+
+    return view('job.show', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
     return view('contact');
 });
+

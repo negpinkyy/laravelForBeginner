c555f19a24659f4dea6ef6c3d0166af25e311d02
pinky winky <mac@MACs-MacBook-Pro.local>
2024-09-18 15:20:23 +0100

    Routes Reloaded

--- routes/web.php
+++ routes/web.php
@@ -1,96 +1,28 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::resource('jobs', JobController::class);
+Route::view('/contact', 'contact');
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+// Route::controller(JobController::class)->group(function (){
 
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-//Stores
-Route::Post('/jobs/', function () {
-    request()->validate([
-        'title' =>['required','max:3'],
-       'salary' =>['required']
-    ]);
-    request()->validate([
-        'title' =>['required','min:3'],
-       'salary' =>['required']
-    ]);
-   Job::create([
-    'title'=>request('title'),
-    'employer_id'=>1,
-    'salary'=>request('salary'),
-   ]);
-   return redirect('/jobs');
-});
-
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-
-    return view('jobs.show', ['job' => $job]);
-});
-//Edit
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = Job::find($id);
-    return view('jobs.edit', ['job' => $job]);
-});
-
-
-//update
-// Update
-Route::patch('/jobs/{id}', function ($id) {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    // authorize (On hold...)
-
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-
-    return redirect('/jobs/' . $job->id);
-});
-
-
-//delete
-Route::delete('/jobs/{id}', function ($id) {
-    //authorize
-    //if the user is not authorized, redirect to home page
-    //if(Auth::id()!=$job->employer_id) return redirect('/');
- //Find the job and delete it
-
- //Delete the job
- $job = Job::findOrFail($id);
- $job->delete();
-    //Redirect
-
-    return redirect('/jobs');
-
-    //redirect
-    //return redirect()->route('jobs.show');
+//     Route::get('/jobs',  'index');
+//     Route::get('/jobs/create','create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::Post('/jobs/','store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}','destroy');
+// });
 
 
 
-});
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+// Route::resource('jobs', JobController::class,[
+//     'except' => ['show']
+// ]);
 
